8f571284574302a4dd674e2ced71ee5dbdeee4b0
Advit <77945990+advitarora135@users.noreply.github.com>
2022-01-22 22:00:51 -0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,53 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+// Your web app's Firebase configuration
+const firebaseConfig = {
+      apiKey: "AIzaSyD6PVsjoATz-HkTgviTv_hVIGXIeabMC0c",
+      authDomain: "kwitter-80410.firebaseapp.com",
+      projectId: "kwitter-80410",
+      storageBucket: "kwitter-80410.appspot.com",
+      messagingSenderId: "958165000115",
+      appId: "1:958165000115:web:05a56180f01af2fb6cf5c6"
+    };
+    
+// Initialize Firebase 
+firebase.initializeApp(firebaseConfig);
+
+ user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="Welcome " + user_name + "!";
+
+function addRoom()
+{
+      room_name = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+      localStorage.setItem("room_name", room_name);
+      window.location="kwitter_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      });});}
+getData();
+function getData2() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+      Room_names = childKey;
+     });});}
+getData();
+
+console.log("Room Name - " + Room_names);
+row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+Room_names+"</div><hr>";
+document.getElementById("output").innerHTML += row;
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+function logout(){
+      user_name = "";
+      localStorage.setItem("user_name",user_name);
+      window.location = "index.html";
+  }
